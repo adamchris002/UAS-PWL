edd6b6b1133dde61d715ec682ca1141f02642d79
Adam Christoper <adamchris002@gmail.com>
2022-12-29 21:08:13 +0700

    rapih2 lagi

--- interface/src/App.js
+++ interface/src/App.js
@@ -1,10 +1,8 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Component/home";
 import Login from "./Component/login";
 import Register from "./Component/register";
 import Details from "./Component/details";
 import React from "react";
-import axios from "axios";
 
 class App extends React.Component {
   constructor(props) {
--- interface/src/Component/Login.js
+++ interface/src/Component/Login.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 import React from "react";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { Form, Button } from "react-bootstrap";
 
 
